80aff1b63b186f7762e4f22684c058d5911615e0
sucer <root@sucer.pc>
2019-03-15 18:17:41 +0800

    update sasy project

--- clean.sh
+++ clean.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 root_dir=$(cd `dirname $0`; pwd)
+echo "root_dir:" ${root_dir}
+echo ""
+rm -rf ./project/Debug/ ./project/x64/ ./project/.vs ./project/project.opensdf ./project/project.sdf ./project/project.VC.db
 
 cd ./game_room/
 make clean
