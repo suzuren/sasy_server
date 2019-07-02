235999804b07be9894abed0cfcd99178c74c4bbe
sucer <root@sucer.pc>
2019-07-02 16:18:15 +0800

    http email

--- http_module/http_main.lua
+++ http_module/http_main.lua
@@ -3,24 +3,24 @@ local ltls = require "ltls.c"
 
 skynet.start(function()
 	skynet.error("Server start")
-	skynet.newservice("fortesthttp")
+	--skynet.newservice("fortesthttp")
 	--skynet.newservice("forsimpleweb","agent","https")
-	--local emaild = skynet.uniqueservice("emaild")
-	--[[
-	mail_opts = {
+	local emaild = skynet.uniqueservice("emaild")
+	
+	local mail_opts = {
 		 host = 'smtp.qq.com',
 		 port = 465,
 		 user = "devsoft@foxmail.com",
-		 token = "tbnzxdczqbzrdidi",
-	},
+		 token = "fortbnzxdczqbzrdidiest",
+	}
 	local email = {
 		 from    = "devsoft@foxmail.com",
 		 to      = "tifanys@qq.com",
 		 subject = "test tail",
-		 html    = "hello world."
+		 content = "hello world."
 	}
-	]]
-	--skynet.call(emaild, "lua", "send_email", email, mail_opts)
+	
+	skynet.call(emaild, "lua", "send_email", email, mail_opts)
 
 	skynet.exit()
 end)
